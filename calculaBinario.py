21b72da77a93939d13f001c44773281c4756edf2
Eduardo <ligadajusticadxc@gmail.com>
2021-12-08 15:11:30 -0300

    apagando o bagulho

--- calculaBinario.py
+++ calculaBinario.py
@@ -25,31 +25,5 @@ def calculaMascara(ipBinario): #Recebe um ip em string
     rede = list(ipBinario) #Converte ele para lista
     mascara = 32 - mask #Calculo para descobrir quantos 0 substituir nos octetos
     num = "0" * mascara # Atribuindo os zeros a uma variável
-    '''
-    rede.reverse()
-    for i in range(len(num)):
-        rede[i] = "0"
-    
-    rede.reverse()
-    '''
-    for i in range(len(num)): #Faz laço com o número de zeros, removendo o último item da lista
-        rede.pop(-1)
-    
-    rede = "".join(rede) #Transfoma a lista  com o itens removidos em string
-    rede = rede + num #Concatera os zeros no final da string
-    
-    return rede #Retorna uma um ip em string
-
-def transformaDecimal(numeroBinario): #Recebe uma sitring de 1 octeto do número binario
-    numDecimal = [] #Cria uma lista vazia
-    pot = len(numeroBinario) - 1 # Calcula o numero de elementos -1 para a exponenciação
-
-    for i in list(numeroBinario): #Transfoma a string binario em lista, e faz um laço para  percorrer cada item
-        num = int(i) # Transforma o item corrente em inteiro
-        num = num *  (2 ** pot) #Faz o calculo do primeiro numero para decimal
-        pot -= 1 # Reduz 1 na potencia
-        numDecimal.append(num) #Atribui o primeiro valor em uma lista
-    
-    return sum(numDecimal) #Soma toda a lista para retornar o número decimal
-
+ 
 
