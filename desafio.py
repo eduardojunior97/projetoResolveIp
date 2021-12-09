32ea0b0a0b4ed3a74f20d5ac5b51015daa8fb765
Justo <karina.justo@dxc.com>
2021-12-09 13:34:48 -0300

    remoção lol.txt e ajuste desafio.py

--- desafio.py
+++ desafio.py
@@ -66,7 +66,3 @@ print('-'*65)
 Default_gateway = str(Adec) +'.'+ str(Bdec) +'.'+ str(Cdec) +'.'+ str(Ddec +1)
 print('Default Gateway:', Default_gateway)
 print('-'*65)
-
-# ALTERAÇÃO PARA TESTE
-
-
