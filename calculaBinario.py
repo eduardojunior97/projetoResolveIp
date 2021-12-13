b1246e36997236e3124f59d34ec1911ab75539b4
eduardo <e.juniordonascimento@dxc.com>
2021-12-13 11:20:26 -0300

    Removendo comentarios

--- calculaBinario.py
+++ calculaBinario.py
@@ -72,8 +72,3 @@ Ddecimal = transformaDecimal(Ddecimal)
 
 print("Esse e o id da rede: {}.{}.{}.{}/{}".format(Adecimal,Bdecimal,Cdecimal,Ddecimal,mask))
 print("Esse e o Default gateway: {}.{}.{}.{}".format(Adecimal,Bdecimal,Cdecimal,Ddecimal + 1))
-
-
-#converte Calcula binario
-#alteração feita pelo vscode
-#testando alterações hoje
